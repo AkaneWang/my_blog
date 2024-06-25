b03e52ea9d12715974c8bddc1c625dec329d1a4a
akane <akane@akanedeMacBook-Air.local>
2024-06-25 22:45:11 +0900

    implement shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
-    // <main>
-    //   <div id="sideNav" className="fixed bg-sky-100 h-full w-[20rem]">
-    //     <div className="text-4xl text-sky-950 flex justify-center my-10">
-    //       my Blog
-    //     </div>
-    //     <div className="text-xl text-sky-700 flex h-full flex-col items-center gap-4 my-40">
-    //       <nav className="hover:text-slate-950">Top</nav>
-    //       <nav className="hover:text-slate-950">About</nav>
-
-    //       <nav>Feed</nav>
-    //       <nav>Contact</nav>
-    //     </div>
-    //   </div>
-    // </main>
+    <main>
+      <h1>Hello</h1>
+    </main>
   );
 }
