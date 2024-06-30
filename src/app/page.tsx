6c77f12a92513271398b12be6fe797a506c12f1c
akane <akane@akanedeMacBook-Air.local>
2024-06-30 21:10:33 +0900

    change the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="md:ml-[20rem]">
-      <h1>Home</h1>
+      <h1 className="text-4xl font-bold mb-4 pl-2 text-sky-950">
+        Welcome to My Blog
+      </h1>
     </main>
   );
 }
