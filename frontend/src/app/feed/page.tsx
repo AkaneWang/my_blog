16141ac99a3bdf579bee7ca8eec5edc8e1c48003
akane <akane@akanedeMacBook-Air.local>
2024-07-14 18:24:29 +0900

    call an endpoint:api/posts

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -1,7 +1,62 @@
+"use client";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { useState, useEffect } from "react";
+
+interface Post {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
+
 export default function Feed() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/api/posts/");
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (err) {
+        setError("Error fetching posts. Please try again later.");
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <main className="ml-[20rem]  h-lvh">
-      <h1>Feed</h1>
+    <main className="md:ml-[20rem] h-lvh">
+      <div className="flex flex-col justify-center items-center gap-8 mt-4">
+        {posts.map((post, _) => {
+          return (
+            <Card>
+              <CardHeader>
+                <CardTitle>{post.title}</CardTitle>
+                <CardDescription>{post.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          );
+        })}
+      </div>
     </main>
   );
 }
