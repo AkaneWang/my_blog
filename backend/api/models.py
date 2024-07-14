9aaed2759350e13fbca5c411c3849870c0893150
akane <akane@akanedeMacBook-Air.local>
2024-07-14 17:57:14 +0900

    add endpoints:Post model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+    title = models.CharField(max_length=50)
+    description = models.TextField(validators=[MinLengthValidator(10)])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,8 @@
+# urls.py
 from django.urls import path
-from rest_framework.routers import DefaultRouter 
+from .views import PostListCreateView, PostRetrieveUpdateDestroyView
 
 urlpatterns = [
+    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-retrieve-update-destroy'),
 ]
